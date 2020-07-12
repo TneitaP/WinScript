768060c06867945acdb281e205dba79b29ee4006
TneitaP <zzmpassion@gmail.com>
2020-07-12 11:24:23 +0800

    add pytorch-env

--- 关闭所有非系统进程.bat
+++
@@ -1,4 +0,0 @@
-tasklist >>pslist.txt
-　　findstr /i /v "System smss.exe csrss.exe winlogon.exe services.exe lsass.exe svchost.exe conime.exe explorer.exe cmd.exe wmiprvse.exe Userinit.exe taskkill.exe" pslist.txt >>npslist.txt
-　　for /f %%a in (npslist.txt) do taskkill /f /t /im %%a
-　　IF ERRORLEVEL 1 NTSD -c q -P %%a
